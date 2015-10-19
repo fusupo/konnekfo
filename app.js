0a25c4cb3b04a78e225f408cd64e8b74df5c8823
Marc Christophe <m@xtof.net>
2015-10-19 12:03:16 -0700

    minor tweak to app.js, jsut wanted to print the current port to console on launch

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 
 var port = process.env.PORT || '3000';
 
 app.use(express.static(__dirname + '/'));
 
-app.listen(port);
+http.listen(port, function() {
+  console.log('listening on *:' + port);
+});
