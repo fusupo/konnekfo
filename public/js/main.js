0ad462512aa8561f8f7fa4b27277af1b98b02603
Marc Christophe <fusupo@gmail.com>
2015-11-07 19:08:57 -0800

    use strict

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,54 @@
+"use strict";
+
 var Game = function() {
 
-  this.board = new Board();
-  this.view = new View();
-  this.view.drawBoard();
+    this.board = new Board();
+    this.view = new View();
+    this.view.drawBoard();
 
-  var p1 = new CPUPlayerClI(2);
-  var p2 = /*new Player(2);*/ new Player(1);
+    var p1 = new CPUPlayerClI(2);
+    var p2 = /*new Player(2);*/ new Player(1);
 
-  var gameOver = false;
-  var currPlayer = p2;
+    var gameOver = false;
+    var currPlayer = p2;
 
-  var winner = 'none';
+    var winner = 'none';
 
-  this.commitMove = function(colIdx) {
+    this.commitMove = function(colIdx) {
 
-    console.log('commit move', colIdx);
-    socket.emit('commit move', colIdx);
+        console.log('commit move', colIdx);
+        socket.emit('commit move', colIdx);
 
-    // if (currPlayer === p1) {
-    //   this.board.move(colIdx, p1.id);
-    //   currPlayer = p2;
-    // } else {
-    //   this.board.move(colIdx, p2.id);
-    //   currPlayer = p1;
-    // }
+        // if (currPlayer === p1) {
+        //   this.board.move(colIdx, p1.id);
+        //   currPlayer = p2;
+        // } else {
+        //   this.board.move(colIdx, p2.id);
+        //   currPlayer = p1;
+        // }
 
-    // this.view.addPiece(colIdx, 6 - (this.board.getNextRowIdx(colIdx) - 2), currPlayer.id ^ 0b11, (function () {
-    //   var winningDirection = this.board.hasWinner();
-    //   if (winningDirection) {
-    //     alert(this.board.winner + ' won! ' + winningDirection);
-    //   } else {
-    //     currPlayer.promptMove(this);
-    //   }
-    // }).bind(this));
+        // this.view.addPiece(colIdx, 6 - (this.board.getNextRowIdx(colIdx) - 2), currPlayer.id ^ 0b11, (function () {
+        //   var winningDirection = this.board.hasWinner();
+        //   if (winningDirection) {
+        //     alert(this.board.winner + ' won! ' + winningDirection);
+        //   } else {
+        //     currPlayer.promptMove(this);
+        //   }
+        // }).bind(this));
 
-  };
+    };
 
-  currPlayer.promptMove(this);
+    currPlayer.promptMove(this);
 
 };
 
 //////////////////////////////////////////////////////////// INIT
 
 window.onload = function() {
-  this.socket = io();
-  this.socket.on('confirm player', function(id){
-    console.log('According to the server, I am player #' + id);
-  });
-  var g = new Game();
+    this.socket = io();
+    this.socket.on('confirm player', function(id){
+        console.log('According to the server, I am player #' + id);
+    });
+    var g = new Game();
 };
+gg
