c4bc459b0278d328395f243c6d486e09307e9444
Marc Christophe <m@xtof.net>
2015-11-11 12:24:46 -0800

    hiding game states, making menu click sensitive

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,23 @@
-var Game = function() {
-
-  this.board = new Board();
-  this.view = new View();
-  this.view.drawBoard();
-
-  var p1 = new CPUPlayerClI(2);
-  var p2 = /*new Player(2);*/ new Player(1);
-
-  var gameOver = false;
-  var currPlayer = p2;
-
-  var winner = 'none';
-
-  this.commitMove = function(colIdx) {
-
-    console.log('commitmove', colIdx);
-    socket.emit('commit move', colIdx);
-
-    // if (currPlayer === p1) {
-    //   this.board.move(colIdx, p1.id);
-    //   currPlayer = p2;
-    // } else {
-    //   this.board.move(colIdx, p2.id);
-    //   currPlayer = p1;
-    // }
-
-    // this.view.addPiece(colIdx, 6 - (this.board.getNextRowIdx(colIdx) - 2), currPlayer.id ^ 0b11, (function () {
-    //   var winningDirection = this.board.hasWinner();
-    //   if (winningDirection) {
-    //     alert(this.board.winner + ' won! ' + winningDirection);
-    //   } else {
-    //     currPlayer.promptMove(this);
-    //   }
-    // }).bind(this));
-
-  };
-
-  currPlayer.promptMove(this);
-
-};
-
-//////////////////////////////////////////////////////////// INIT
-
 window.onload = function() {
+
   this.socket = io();
   this.socket.on('confirm player', function(id) {
     console.log('According to the server, I am player #' + id);
   });
 
-  var g = new Game();
+  $('#game').hide();
+  $('#conclusion').hide();
+
+  //var g = new Game();
+  $('#vs-human-local').click(function() {
+    console.log('vs-human-local');
+  });
+
+  $('#vs-human-network').click(function() {
+    console.log('vs-human-network');
+  });
+
+  $('#vs-computer').click(function() {
+    console.log('vs-computer');
+  });
 };
