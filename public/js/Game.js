344bb4fda64303bf40f0e29d45d77f766076e408
Marc Christophe <m@xtof.net>
2015-11-11 09:52:43 -0800

    breaking Game into its own file

---
+++ public/js/Game.js
@@ -0,0 +1,41 @@
+var Game = function() {
+
+  this.board = new Board();
+  this.view = new View();
+  this.view.drawBoard();
+
+  var p1 = new CPUPlayerClI(2);
+  var p2 = /*new Player(2);*/ new Player(1);
+
+  var gameOver = false;
+  var currPlayer = p2;
+
+  var winner = 'none';
+
+  this.commitMove = function(colIdx) {
+
+    console.log('commitmove', colIdx);
+    socket.emit('commit move', colIdx);
+
+    // if (currPlayer === p1) {
+    //   this.board.move(colIdx, p1.id);
+    //   currPlayer = p2;
+    // } else {
+    //   this.board.move(colIdx, p2.id);
+    //   currPlayer = p1;
+    // }
+
+    // this.view.addPiece(colIdx, 6 - (this.board.getNextRowIdx(colIdx) - 2), currPlayer.id ^ 0b11, (function () {
+    //   var winningDirection = this.board.hasWinner();
+    //   if (winningDirection) {
+    //     alert(this.board.winner + ' won! ' + winningDirection);
+    //   } else {
+    //     currPlayer.promptMove(this);
+    //   }
+    // }).bind(this));
+
+  };
+
+  currPlayer.promptMove(this);
+
+};
