a8939ea18f38976bbc6c4720d98aba7c9090435d
Marc Christophe <m@xtof.net>
2015-11-15 14:16:24 -0800

    cleanup/working towards network sessions

--- game/Main.js
+++ game/Main.js
@@ -7,16 +7,21 @@ var View = require('./View.js');
 window.onload = function() {
 
   this.game = 'game';
-  this.socket = io();
-  this.socket.on('confirm player', function(id) {
-    console.log('According to the server, I am player #' + id);
-  });
+  // this.socket = io();
+  // this.socket.on('confirm player', function(id) {
+  //   console.log('According to the server, I am player #' + id);
+  // });
 
   $('#connect').hide();
   $('#game').hide();
   $('#conclusion').hide();
 
   var $vsHumanLocal = $('#vs-human-local');
+  var $vsHumanNetwork = $('#vs-human-network');
+  var $vsComputer = $('#vs-computer');
+  var $networkNew = $('#network-new');
+  var $networkConnect = $('#network-connect');
+
   $vsHumanLocal.click(function() {
     $('#game').show();
     $('#menu').hide();
@@ -24,7 +29,57 @@ window.onload = function() {
     var p1 = new Players.Player(1, view);
     var p2 = new Players.Player(2, view);
     view.drawBoard();
-    window.game = new Game(p1, p2);
+    var game = new Game(p1, p2);
+    game.moveCommitted = function(colIdx) {
+      view.addPiece(colIdx, 6 - (game.board.getNextRowIdx(colIdx) - 2),
+        game.currPlayer.id ^ 3, //0 b11,
+        function() {
+          var winningDirection = game.board.hasWinner();
+          if (winningDirection) {
+            alert(game.board.winner + ' won! ' + winningDirection);
+          }
+        });
+    };
+  });
+
+  $vsHumanNetwork.click(function() {
+    $('#connect').show();
+    $('#menu').hide();
+
+    //window.game = new NetworkGame();
+  });
+
+  $networkNew.click(function() {
+    var sessionId;
+    $.get("/session/new", function(data, status) {
+      sessionId = data;
+      var socket = io(window.location.href + sessionId);
+      $('#game').show();
+      $('#connect').hide();
+
+      // window.game.new(function() {
+      //   console.log('new game created!');
+      // });
+
+    });
+  });
+
+  $networkConnect.click(function() {
+    //   $('#game').show();
+    //   $('#connect').hide();
+    //   window.game.connect('some connection id', function() {
+    //     console.log('connected');
+    //   });
+  });
+
+  $vsComputer.click(function() {
+    $('#game').show();
+    $('#menu').hide();
+    var view = new View();
+    var p1 = new Players.Player(1, view);
+    var p2 = new Players.CPUPlayerClI(2);
+    view.drawBoard();
+    window.game = new Game(p1, p2); //new LocalGame(new Player(1), new CPUPlayerClI(2));
     window.game.moveCommitted = function(colIdx) {
       view.addPiece(colIdx, 6 - (window.game.board.getNextRowIdx(colIdx) - 2),
         window.game.currPlayer.id ^ 3, //0 b11,
@@ -36,33 +91,4 @@ window.onload = function() {
         });
     };
   });
-
-  // $('#vs-human-network').click(function() {
-  //   $('#connect').show();
-  //   $('#menu').hide();
-  //   window.game = new NetworkGame();
-  // });
-
-  // $('#vs-computer').click(function() {
-  //   $('#game').show();
-  //   $('#menu').hide();
-  //   window.game = new LocalGame(new Player(1), new CPUPlayerClI(2));
-  // });
-
-  // $('#network-new').click(function() {
-  //   $('#game').show();
-  //   $('#connect').hide();
-  //   window.game.new(function() {
-  //     console.log('new game created!');
-  //   });
-  // });
-
-  // $('#network-connect').click(function() {
-  //   $('#game').show();
-  //   $('#connect').hide();
-  //   window.game.connect('some connection id', function() {
-  //     console.log('connected');
-  //   });
-  // });
-
 };
