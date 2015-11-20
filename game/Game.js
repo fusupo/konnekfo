7274453b6a0cb4b7f691df70ff442b0f6c9eb510
Marc Christophe <m@xtof.net>
2015-11-20 11:47:16 -0800

    keeping tally in Game, keeping gamestate (re)initialization DRY

--- game/Game.js
+++ game/Game.js
@@ -6,8 +6,8 @@ module.exports = function(p1, p2) {
 
   console.log('GAME INIT');
 
-  this.board = new Board();
-  var firstToPlay = this.currPlayer = p1;
+  this.winTally = [0, 0, 0];
+  var firstToPlay; // = this.currPlayer = p1;
 
   this.commitMove = function(colIdx) {
     if (!this.board.isColFull(colIdx)) {
@@ -28,6 +28,12 @@ module.exports = function(p1, p2) {
         this.currPlayer.promptMove(this);
       }
 
+      if (winningDirection) {
+        this.winTally[(this.currPlayer.id ^ 3) - 1] ++;
+      } else if (this.board.isBoardFull()) {
+        this.winTally[2] ++;
+      }
+
       return true;
     } else {
       this.currPlayer.promptMove(this);
@@ -35,14 +41,21 @@ module.exports = function(p1, p2) {
     }
   };
 
-  this.currPlayer.promptMove(this);
-
   this.reset = function() {
-    console.log('reset the fuggin game!!');
+
     this.board = new Board();
-    // switch who starts every other game...(now I'm gonna have to keep a tally of games won overall #eyeROll)
-    firstToPlay = this.currPlayer = firstToPlay === p1 ? p2 : p1;
+
+    // switch who starts every other game...
+    if (!firstToPlay) {
+      firstToPlay = this.currPlayer = p1;
+    } else {
+      firstToPlay = this.currPlayer = firstToPlay === p1 ? p2 : p1;
+    }
+
     this.currPlayer.promptMove(this);
+
   };
+
+  this.reset();
 
 };
